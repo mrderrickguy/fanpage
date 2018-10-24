b57ff13d936bee9bb85eeea160ae188a0014a116
mrderrickguy <zakiahdg@gmail.com>
2018-10-24 12:32:27 -0500

    Updating fan page

--- fan.js
+++ fan.js
@@ -1,6 +1,29 @@
 $(document).ready(function(){
-    $('.carousel').carousel({
-  interval: 2000
-})
-  
+	document.getElementById("raplink").onmouseover = function() {mouseOver()};
+document.getElementById("raplink").onmouseout = function() {mouseOut()};
+
+
+function mouseOver() {
+    document.getElementById("raplink").style.color = "yellow";
+}
+function mouseOut() {
+    document.getElementById("raplink").style.color = "white"; 
+}
+// function mouseOut() {
+//     document.getElementById("raplink").style.color = "black";
+// }
+});
+
+
+$(document).ready(function(){
+document.getElementById("zillalink").onmouseout = function() {mouseOut()};
+document.getElementById("zillalink").onmouseover = function() {mouseOver()};
+
+function mouseOver() {
+    document.getElementById("zillalink").style.color = "black";
+}
+
+function mouseOut() {
+    document.getElementById("zillalink").style.color = "blue";
+}
 });
